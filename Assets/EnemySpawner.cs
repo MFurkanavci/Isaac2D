0251ed07586e9062ca2b26e1353039a9e01298ba
Muhammet Furkan AVCI <furkanavci25@gmail.com>
2024-08-12 20:40:36 +0300

    Yeni değişiklikler.

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -13,7 +13,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject Room;
     private Player player;
     private Vector2 roomSize;
-    private Transform spawnPoint;
+    private Vector3 spawnPoint;
     public float distanceFromPlayer;
     public int totalEnemies;
     public int enemiesPerGroup;
@@ -25,22 +25,23 @@ public class EnemySpawner : MonoBehaviour
         //We will get the room size and spawn point
         player = GameObject.Find("Player").GetComponent<Player>();
         roomSize = Room.GetComponent<SpriteRenderer>().bounds.size;
-
         //We will spawn the first group of enemies
         SpawnGroup();
     }
 
     void SetNewSpawnPoint()
     {
-        //We will get a random position within the room but away from the player
-        Vector2 randomPosition = new Vector2(Random.Range(-roomSize.x / 2, roomSize.x / 2), Random.Range(-roomSize.y / 2, roomSize.y / 2));
-        if (Vector2.Distance(randomPosition, player.transform.position) < distanceFromPlayer)
+        //We will set the new spawn point
+        spawnPoint = new Vector3(Random.Range(-roomSize.x / 2, roomSize.x / 2), Random.Range(-roomSize.y / 2, roomSize.y / 2), 0);
+        //If the spawn point is too close to the player, we will set a new one
+        if (Vector2.Distance(spawnPoint, player.transform.position) < distanceFromPlayer)
         {
             SetNewSpawnPoint();
         }
         else
         {
-            spawnPoint.position = randomPosition;
+            //We will set the new spawn point
+            spawnPoint = new Vector3(spawnPoint.x + Room.transform.position.x, spawnPoint.y + Room.transform.position.y, 0);
         }
     }
 
@@ -60,19 +61,17 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnGroup()
     {
-        //We will get a random number of enemies to spawn in the group
-        int enemiesToSpawn = Random.Range(3, 6);
-        //We will get a random spawn point for each enemy in the group
-        for (int i = 0; i < enemiesToSpawn; i++)
-        {
-            SetNewSpawnPoint();
-            GameObject enemy = objectPool.GetFromPool(enemyTag, spawnPoint.position, Quaternion.identity);
-            enemy.GetComponent<Enemy>().OnSpawn();
-        }
-        //We will decrease the total enemies by the number of enemies spawned
-        totalEnemies -= enemiesToSpawn;
         //We will set the next spawn time
         nextSpawnTime = Time.time + spawnRate;
+        //We will set the new spawn point
+        SetNewSpawnPoint();
+        //We will spawn the enemies
+        for (int i = 0; i < enemiesPerGroup; i++)
+        {
+            GameObject enemy = objectPool.GetFromPool(enemyTag, spawnPoint, Quaternion.identity);
+            enemy.GetComponent<Enemy>().OnSpawn();
+            totalEnemies--;
+        }
     }
 
 }
