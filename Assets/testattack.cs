69a63f2b090ff0b20e1e68f119a5de026cc3944a
Saliheyp <m.saliheyupoglu@gmail.com>
2024-11-05 21:33:34 +0300

    lastChange

--- Assets/testattack.cs
+++ Assets/testattack.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class testattack : MonoBehaviour
 {
-   
+    Transform thyatisChild;
+
+    void Start()
+    {
+
+        thyatisChild = gameObject.transform.Find("yarakfurkan/Thyatis");
+        if (thyatisChild != null)
+        {
+            thyatisChild.GetComponent<Animator>().SetTrigger("Death");
+        }
+        else
+        {
+            Debug.Log("Belirtilen child bulunamadı.");
+        }
+    }
+
+
 }
